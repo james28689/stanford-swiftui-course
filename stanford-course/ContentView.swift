2f679312831465de4352d8c177fe5ddf638a3926
James Watling <james.watling@gmail.com>
2021-01-10 13:41:40 +0000

    Initial Commit

---
+++ stanford-course/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stanford-course
+//
+//  Created by James Watling on 10/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ stanford-course/stanford_courseApp.swift
@@ -0,0 +1,17 @@
+//
+//  stanford_courseApp.swift
+//  stanford-course
+//
+//  Created by James Watling on 10/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct stanford_courseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
